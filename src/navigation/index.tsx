26549c1475e9bacece13d6f5c9056c0ae3defa99
Patrick <patrickcurran85@gmail.com>
2025-01-28 12:46:05 -0700

    added setup and intial screens

---
+++ src/navigation/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import WelcomeScreen from '../screens/WelcomeScreen';
+import WorkoutSelectionScreen from '../screens/WorkoutSelectionScreen';
+
+const Stack = createStackNavigator();
+
+const Na
